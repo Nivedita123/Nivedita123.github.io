2fda820ae872fbbc15d7ff0291efb28cc361c9c4
Nivedita Gautam <niveditagautam000@gmail.com>
2019-09-16 12:29:10 -0500

    Updates

---
+++ precache-manifest.86a1b27cd99c5244a780a7d9f51efa9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4092b6c86930bf94046a9623e8ae7ee1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c0e5db7447f9cd641f1",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "a4e5cf258b45ce8cdd7a",
+    "url": "/static/css/main.10848714.chunk.css"
+  },
+  {
+    "revision": "8c0e5db7447f9cd641f1",
+    "url": "/static/js/2.e7ecf676.chunk.js"
+  },
+  {
+    "revision": "a4e5cf258b45ce8cdd7a",
+    "url": "/static/js/main.3a26f4ef.chunk.js"
+  },
+  {
+    "revision": "40b0b67256a48a5a54f3",
+    "url": "/static/js/runtime~main.9c5383ea.js"
+  },
+  {
+    "revision": "57fd94a1910f7fea0eaf90a4e1e1f6e5",
+    "url": "/static/media/bg.57fd94a1.jpg"
+  },
+  {
+    "revision": "2718019012152321d36515c317373578",
+    "url": "/static/media/footer-bg.27180190.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.842eb2ff0c974cab8b5bdfa78080e0fa.js"
+  "/precache-manifest.86a1b27cd99c5244a780a7d9f51efa9b.js"
 );
 
 self.addEventListener('message', (event) => {
